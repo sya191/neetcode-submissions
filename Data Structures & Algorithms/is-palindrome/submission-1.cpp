f7f06e77b17a1c91dd6082a627921a7a10fda589
Samuel Ye <Samuel.ye915@gmail.com>
2026-05-13 13:22:55 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // convert to lower
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] += 'a' - 'A';
+            }
+        }
+        // keep track of two pointers
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (!std::isalnum(s[start])) {
+                start++;
+                continue;
+            }
+            if (!std::isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (s[start++] != s[end--]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
